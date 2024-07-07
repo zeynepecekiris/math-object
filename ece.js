fb65ebda6d7175ab5767412d3f63e9383f374895
zeynepecekiris <zeynep.ece933@gmail.com>
2024-07-07 19:21:52 +0300

    first commit

---
+++ ece.js
@@ -0,0 +1,17 @@
+console.log(Math);
+console.log(Math.PI);
+
+
+let sayi =5.7;
+
+console.log(Math.round(sayi));
+//round en yakin sayiya yuvarlar
+console.log(Math.floor(sayi));//1 alta yuvarlar
+console.log(Math.ceil(sayi)); //1 uste yuvarlar
+console.log(Math.trunc(sayi)); //decimal kismi ucur
+
+
+const rastgele=Math.random(); //kafadan sayi atar
+
+console.log(rastgele);
+console.log(Math.round(rastgele*100));
